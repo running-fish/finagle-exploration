0c3eaf93bf263657c85269901071d52aded6cdaa
Jake Meier <jmeier@tendrilinc.com>
2015-03-22 12:24:14 -0600

    Remove custom counters; rely on finagle stats

--- src/main/scala/HttpClient.scala
+++ src/main/scala/HttpClient.scala
@@ -1,10 +1,9 @@
 import java.util.concurrent.atomic.AtomicInteger
-import java.util.logging.{Level, ConsoleHandler, Logger}
+import java.util.logging.{Level, Logger}
 
-import com.twitter.finagle.FailFastException
 import com.twitter.finagle.builder.ClientBuilder
 import com.twitter.finagle.http.Http
-import com.twitter.finagle.stats.{SummarizingStatsReceiver, JavaLoggerStatsReceiver}
+import com.twitter.finagle.stats.{SummarizingStatsReceiver}
 import com.twitter.util.{Duration, Future}
 import org.jboss.netty.handler.codec.http.HttpVersion._
 import org.jboss.netty.handler.codec.http.HttpMethod._
@@ -18,22 +17,18 @@ object HttpClient {
 
   def main(args: Array[String]): Unit = {
     val client = makeVanillaClient()
-    val countdownHook = new AtomicInteger(20001)
-    val successCount = new AtomicInteger(0)
-    val errorCount = new AtomicInteger(0)
+    val countdownHook = new AtomicInteger(10000)
 
-    Range(1, countdownHook.get()).foreach{i =>
+    Range(0, countdownHook.get()).foreach{i =>
       val request = new DefaultHttpRequest(HTTP_1_1, GET, "/")
       val responseFuture: Future[HttpResponse] = client(request)
       responseFuture.onSuccess {response =>
-        successCount.incrementAndGet()
         val responseCount = countdownHook.decrementAndGet()
         if (responseCount == 1) {
           shutdown()
         }
       }
       responseFuture.onFailure { ex =>
-        errorCount.incrementAndGet()
         ex match {
           case f: com.twitter.finagle.FailedFastException => {/* ignore; no message */}
           case m: Exception => ex.printStackTrace()
@@ -46,9 +41,6 @@ object HttpClient {
       }
 
       def shutdown(): Unit = {
-        logger.info("SuccessCount: %s".format(successCount.intValue()))
-        logger.info("ErrorCount: %s".format(errorCount.intValue()))
-        logger.info("")
         logger.info(stats.summary())
         client.release()
       }
